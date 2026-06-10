f00b5512b94910135def3828f3cd1955772d73d0
Huynh Le Nhat Thanh <nteesouthside420@gmail.com>
2026-06-11 05:31:09 +0700

    fix esbuild build on render

--- artifacts/api-server/build.mjs
+++ artifacts/api-server/build.mjs
@@ -102,10 +102,7 @@ async function buildAll() {
       "electron",
     ],
     sourcemap: "linked",
-    plugins: [
-      // pino relies on workers to handle logging, instead of externalizing it we use a plugin to handle it
-      esbuildPluginPino({ transports: ["pino-pretty"] })
-    ],
+    plugins: [],
     // Make sure packages that are cjs only (e.g. express) but are bundled continue to work in our esm output file
     banner: {
       js: `import { createRequire as __bannerCrReq } from 'node:module';
